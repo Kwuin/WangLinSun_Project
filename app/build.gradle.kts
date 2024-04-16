a3d9c500de8dc930ce62d99dcbc440db1c877628
yidwang65 <yidwang@bu.edu>
2024-04-15 21:46:55 -0400

    Create profile page and delete some fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.cs506finalproject"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -27,7 +27,9 @@ android {
             )
         }
     }
+
     compileOptions {
+
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -35,8 +37,12 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        viewBinding = true
+        compose = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.11"
+    }
+
 }
 
 dependencies {
@@ -48,11 +54,19 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.compose.ui:ui-tooling-preview-android:1.6.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.customview:customview-poolingcontainer:1.0.0-alpha01")
     val room_version = "2.5.2"
-
+    implementation("androidx.compose.ui:ui:1.1.0-alpha06") // Replace with the desired Jetpack Compose UI version
+    implementation("androidx.compose.material:material:1.1.0-alpha06") // Replace with the desired Jetpack Compose Material version
+    implementation("androidx.activity:activity-compose:1.3.1") // Replace with the desired Activity Compose version
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    // Kotlin standard library
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31") // Replace with the desired Kotlin version
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
