c621657f841cd7e3effeb749473fe764afd6d616
HaoxuanSUN <3404164959@qq.com>
2024-04-17 02:03:07 -0400

    Inter-pages navigation logic implementation (Compose)

--- app/src/main/java/com/example/cs501finalproject/ui/NavigationBar.kt
+++ app/src/main/java/com/example/cs501finalproject/ui/NavigationBar.kt
@@ -2,27 +2,22 @@ package com.example.cs501finalproject.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavController
 import com.example.cs501finalproject.R
 
 
@@ -220,7 +215,7 @@ import com.example.cs501finalproject.R
 
 
 @Composable
-fun NavigationBar(modifier: Modifier = Modifier) {
+fun NavigationBar(navController: NavController, modifier: Modifier = Modifier) {
     val selectedTab = remember { mutableStateOf(0) }
     Row(
         horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
@@ -233,19 +228,31 @@ fun NavigationBar(modifier: Modifier = Modifier) {
         HomeNavigation(
             modifier = Modifier.weight(weight = 0.25f),
             selected = selectedTab.value == 0,
-            onClick = { selectedTab.value = 0 })
+            onClick = {
+                selectedTab.value = 0
+                navController.navigate("home")
+            })
         CalendarNavigation(
             modifier = Modifier.weight(weight = 0.25f),
             selected = selectedTab.value == 1,
-            onClick = { selectedTab.value = 1 })
+            onClick = {
+                selectedTab.value = 1
+                navController.navigate("calendar")
+            })
         MemoriesNavigation(
             modifier = Modifier.weight(weight = 0.25f),
             selected = selectedTab.value == 2,
-            onClick = { selectedTab.value = 2 })
+            onClick = {
+                selectedTab.value = 2
+                navController.navigate("memories")
+            })
         SettingsNavigation(
             modifier = Modifier.weight(weight = 0.25f),
             selected = selectedTab.value == 3,
-            onClick = { selectedTab.value = 3 })
+            onClick = {
+                selectedTab.value = 3
+                navController.navigate("settings")
+            })
     }
 }
 
@@ -317,5 +324,6 @@ fun NavigationItem(modifier: Modifier, isSelected: Boolean, onClick: () -> Unit,
 @Preview(widthDp = 360, heightDp = 70)
 @Composable
 private fun NavigationBarPreview() {
-    NavigationBar(Modifier)
+    val navController = rememberNavController()
+    NavigationBar(navController, Modifier)
 }
