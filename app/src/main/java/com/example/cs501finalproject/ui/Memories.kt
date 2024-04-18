870d1ede7e47c5e08c23e242ea482f0d983ba14e
HaoxuanSUN <3404164959@qq.com>
2024-04-18 02:08:14 -0400

    Memories Page update data

--- app/src/main/java/com/example/cs501finalproject/ui/Memories.kt
+++ app/src/main/java/com/example/cs501finalproject/ui/Memories.kt
@@ -10,17 +10,17 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavController
 import com.example.cs501finalproject.Blog
 import com.example.cs501finalproject.R
 
@@ -36,18 +36,36 @@ data class EventItem(
 
 fun getSampleData(): List<EventItem> {
     return listOf(
-        EventItem(null, "Yesterday", "New Zealand", "NZ holiday", R.drawable.blog_boulders, "\uD83D\uDE0A"),
-        EventItem(null, "1 Week Ago", "Boston", "Busy Day", R.drawable.blog_example, "\uD83D\uDE0A"),
-        EventItem(null, "1 Month Ago", "Australia", "Spring Break", R.drawable.blog_nature_window, "\uD83D\uDE0A"),
-        EventItem(null, "6 Months Ago", "New Zealand", "NZ holiday", R.drawable.blog_boulders, "\uD83D\uDE0A"),
-        EventItem(null, "1 Year Ago", "Boston", "Rainy", R.drawable.blog_example, "\u2614"),
-        EventItem(null, "2 Years Ago", "Boston", "Boring", R.drawable.blog_blank, "\uD83D\uDE22"),
-        EventItem(null, "3 Years Ago", "Boston", "Default Title", R.drawable.blog_blank, "\uD83D\uDE0A"),
-        EventItem(null, "4 Years Ago", "Boston", "Happy Birthday", R.drawable.blog_blank, "\uD83D\uDE0A"),
-        EventItem(null, "5 Years Ago", "Boston", "Busy Weekend", R.drawable.blog_blank, "\uD83D\uDE0A"),
-        EventItem(null, "6 Years Ago", "Boston", "WOW", R.drawable.blog_blank, "\uD83D\uDE0A"),
-        EventItem(null, "7 Years Ago", "Boston", "Cold", R.drawable.blog_blank, "\uD83D\uDE0A"),
-        EventItem(null, "8 Years Ago", "Boston", "Trip to LA", R.drawable.blog_blank, "\uD83D\uDE0A"),
+        EventItem(null, "Yesterday", "New Zealand", "NZ holiday", R.drawable.blog_example, "\uD83E\uDD29"),
+        EventItem(null, "1 Week Ago", "New Zealand", "NZ holiday", R.drawable.blog_boulders, "\uD83E\uDD29"),
+        EventItem(null, "1 Week Ago", "New Zealand", "NZ holiday", R.drawable.blog_example, "\uD83D\uDCBB"),
+        EventItem(null, "1 Month Ago", "Australia", "Spring Break", R.drawable.blog_nature_window, "\u2708"),
+        EventItem(null, "1 Year Ago", "Boston", "Rainy", R.drawable.blog_boulders, "\u2614"),
+        EventItem(null, "1 Year Ago", "Boston", "Rainy", R.drawable.blog_boulders, "\u2614"),
+        EventItem(null, "1 Year Ago", "Boston", "Rainy", R.drawable.blog_boulders, "\u2614")
+    )
+}
+fun getSampleDataDay(): List<EventItem> {
+    return listOf(
+        EventItem(null, "2024/04/17", "New Zealand", "NZ holiday", R.drawable.blog_example, "\uD83E\uDD29")
+    )
+}
+fun getSampleDataWeek(): List<EventItem> {
+    return listOf(
+        EventItem(null, "2024/04/11", "New Zealand", "NZ holiday", R.drawable.blog_boulders, "\uD83E\uDD29"),
+        EventItem(null, "2024/04/11", "New Zealand", "NZ holiday", R.drawable.blog_example, "\uD83D\uDCBB")
+    )
+}
+fun getSampleDataMonth(): List<EventItem> {
+    return listOf(
+        EventItem(null, "2024/03/18", "Australia", "Spring Break", R.drawable.blog_nature_window, "\u2708"),
+    )
+}
+fun getSampleDataYear(): List<EventItem> {
+    return listOf(
+        EventItem(null, "2023/04/18", "Boston", "Rainy", R.drawable.blog_boulders, "\u2614"),
+        EventItem(null, "2023/04/18", "Boston", "Rainy", R.drawable.blog_boulders, "\u2614"),
+        EventItem(null, "2023/04/18", "Boston", "Rainy", R.drawable.blog_boulders, "\u2614")
     )
 }
 
@@ -57,31 +75,29 @@ fun MemoriesPage(navController: NavController){
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        MemoriesPictureCarousel(modifier = Modifier.weight(2.5f))
-        banner("A day ago")
-        MemoriesListCarousel(modifier = Modifier.weight(1f))
-        banner("A Week ago")
-        MemoriesListCarousel(modifier = Modifier.weight(1f))
-        banner("A Month ago")
-        MemoriesListCarousel(modifier = Modifier.weight(1f))
-        banner("A year ago")
-        MemoriesListCarousel(modifier = Modifier.weight(1f))}
-
+        MemoriesPictureCarousel(modifier = Modifier.weight(3f))
+        Banner("A day Ago")
+        MemoriesListCarouselDay(modifier = Modifier.weight(1f))
+        Banner("A Week Ago")
+        MemoriesListCarouselWeek(modifier = Modifier.weight(1f))
+        Banner("A Month Ago")
+        MemoriesListCarouselMonth(modifier = Modifier.weight(1f))
+        Banner("A Year Ago")
+        MemoriesListCarouselYear(modifier = Modifier.weight(1f))
+    }
 }
 
 @Composable
-fun banner(title: String){
+fun Banner(title: String){
     Box(
         Modifier
             .fillMaxWidth()
             .height(20.dp)
-            .background(color = Color(0xFFD4CADC))
-        ,
+            .background(color = Color(0xFFD4CADC)),
         contentAlignment = Alignment.Center
     ) {
         Text(
             text = title,
-
             style = TextStyle(
                 fontSize = 14.sp,
                 lineHeight = 20.sp,
@@ -93,6 +109,7 @@ fun banner(title: String){
         )
     }
 }
+
 @Composable
 fun MemoriesPictureCarousel(modifier: Modifier = Modifier) {
     LazyRow(
@@ -143,13 +160,49 @@ fun MemoryPictureItem(item: EventItem, modifier: Modifier) {
 
 
 @Composable
-fun MemoriesListCarousel(modifier: Modifier) {
+fun MemoriesListCarouselDay(modifier: Modifier) {
     LazyColumn(
         modifier = modifier.fillMaxSize(),
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(getSampleData()) { item ->
+        items(getSampleDataDay()) { item ->
+            MemoryListItem(item)
+        }
+    }
+}
+@Composable
+fun MemoriesListCarouselWeek(modifier: Modifier) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(getSampleDataWeek()) { item ->
+            MemoryListItem(item)
+        }
+    }
+}
+@Composable
+fun MemoriesListCarouselMonth(modifier: Modifier) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(getSampleDataMonth()) { item ->
+            MemoryListItem(item)
+        }
+    }
+}
+@Composable
+fun MemoriesListCarouselYear(modifier: Modifier) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(getSampleDataYear()) { item ->
             MemoryListItem(item)
         }
     }
@@ -164,7 +217,6 @@ fun MemoryListItem(item: EventItem) {
         // 时间和地点信息区 (左侧 3/10)
         Column(
             modifier = Modifier.weight(3f),
-//            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = item.date,
@@ -198,38 +250,38 @@ fun MemoryListItem(item: EventItem) {
     }
 }
 
-@Composable
-fun Memorylist(title: String, blogs: List<EventItem>){
-    Column(){
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .height(20.dp)
-                .background(color = Color(0xFFD4CADC))
-            ,
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = title,
-
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    lineHeight = 20.sp,
-                    fontWeight = FontWeight(500),
-                    color = Color(0xFF000000),
-                    textAlign = TextAlign.Center,
-                    letterSpacing = 0.1.sp,
-                )
-            )
-        }
-        LazyColumn(){
-            items(blogs){blog ->
-                MemoryListItem(item = blog)
-            }
-        }
-
-    }
-}
+//@Composable
+//fun Memorylist(title: String, blogs: List<EventItem>){
+//    Column(){
+//        Box(
+//            Modifier
+//                .fillMaxWidth()
+//                .height(20.dp)
+//                .background(color = Color(0xFFD4CADC))
+//            ,
+//            contentAlignment = Alignment.Center
+//        ) {
+//            Text(
+//                text = title,
+//
+//                style = TextStyle(
+//                    fontSize = 14.sp,
+//                    lineHeight = 20.sp,
+//                    fontWeight = FontWeight(500),
+//                    color = Color(0xFF000000),
+//                    textAlign = TextAlign.Center,
+//                    letterSpacing = 0.1.sp,
+//                )
+//            )
+//        }
+//        LazyColumn(){
+//            items(blogs){blog ->
+//                MemoryListItem(item = blog)
+//            }
+//        }
+//
+//    }
+//}
 
 @Preview
 @Composable
