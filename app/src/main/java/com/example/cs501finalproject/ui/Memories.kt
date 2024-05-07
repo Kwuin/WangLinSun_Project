a81769f0a2468a0ac932f945d1ab9889f061e732
Kwuin <ljkwuin@gmail.com>
2024-05-07 17:26:28 -0400

    carousel of memories

--- app/src/main/java/com/example/cs501finalproject/ui/Memories.kt
+++ app/src/main/java/com/example/cs501finalproject/ui/Memories.kt
@@ -11,7 +11,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
@@ -99,15 +102,28 @@ fun Banner(title: String){
 
 @Composable
 fun MemoriesPictureCarousel(modifier: Modifier = Modifier) {
-    val blogListViewModel = MemoryBlogListDayAgoViewModel()
-    val blogs = blogListViewModel.blogs.collectAsState(initial = emptyList())
 
+
+    val memoryBlogListDayAgoViewModel = MemoryBlogListDayAgoViewModel()
+    val memoryBlogListWeekAgoViewModel = MemoryBlogListWeekAgoViewModel()
+    val memoryBlogListMonthAgoViewModel = MemoryBlogListMonthAgoViewModel()
+    val memoryBlogListYearAgoViewModel = MemoryBlogListYearAgoViewModel()
+    val dayAgoBlogs = memoryBlogListDayAgoViewModel.blogs.collectAsState(initial = emptyList()).value
+    val weekAgoBlogs = memoryBlogListWeekAgoViewModel.blogs.collectAsState(initial = emptyList()).value
+    val monthAgoBlogs = memoryBlogListMonthAgoViewModel.blogs.collectAsState(initial = emptyList()).value
+    val yearAgoBlogs = memoryBlogListYearAgoViewModel.blogs.collectAsState(initial = emptyList()).value
+
+    val blogs = memoryBlogListDayAgoViewModel.blogs.collectAsState(initial = emptyList())
+    val combinedBlogs = remember { mutableStateOf(listOf<Blog>()) }
+    LaunchedEffect(dayAgoBlogs, weekAgoBlogs, monthAgoBlogs, yearAgoBlogs) {
+        combinedBlogs.value = dayAgoBlogs + weekAgoBlogs + monthAgoBlogs + yearAgoBlogs
+    }
     LazyRow(
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier.fillMaxHeight()
     ) {
-        items(blogs.value) { item ->
+        items(combinedBlogs.value) { item ->
             if (item.photoFileName != null){
                 MemoryPictureItem(item, Modifier.padding(vertical = 8.dp))
             }
@@ -123,20 +139,7 @@ fun MemoryPictureItem(item: Blog, modifier: Modifier) {
         elevation = 4.dp
     ) {
         Column {
-            item?.photoFileName?.let { fileName ->
-                val imageFile = File(fileName)
-                if (imageFile.exists()){
-                    val bitmap = BitmapFactory.decodeFile(fileName).asImageBitmap()
-                    Image(
-                        bitmap = bitmap,
-                        contentDescription = "Selected Image",
-                        modifier = Modifier.fillMaxWidth()
-                            .height(150.dp)
-                    )
-                }else{
-                    Log.d("image File", "non exist")
-                }
-            }
+
             Text(
                 text = item.date.toString(),
                 fontSize = 16.sp,
@@ -156,6 +159,20 @@ fun MemoryPictureItem(item: Blog, modifier: Modifier) {
                     .padding(4.dp),
                 textAlign = TextAlign.Center
             )
+            item?.photoFileName?.let { fileName ->
+                val imageFile = File(fileName)
+                if (imageFile.exists()) {
+                    val bitmap = BitmapFactory.decodeFile(fileName).asImageBitmap()
+                    Image(
+                        bitmap = bitmap,
+                        contentDescription = "Selected Image",
+                        modifier = Modifier.fillMaxWidth()
+                            .height(150.dp)
+                    )
+                } else {
+                    Log.d("image File", "non exist")
+                }
+            }
         }
     }
 }
